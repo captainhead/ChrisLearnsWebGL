234c0ac0467573f1c0c479a1e1129c8b385125e2
Chris Little <chrislittle1986@gmail.com>
2012-05-29 22:04:50 -0400

    Completed Tutorial 9. Randomized colours are reduced to 80%to appear less intense on the lighter default colour of the background.

--- webgl.js
+++ webgl.js
@@ -1,27 +1,106 @@
 var gl;
 
-var cubeVertexPositionBuffer;
-var cubeVertexIndexBuffer;
-var cubeVertexTextureCoordBuffer;
-var cubeVertexNormalBuffer;
-
 var mvMatrixStack = []; // Save and restore previous modelview matrices
 var mvMatrix = mat4.create(); // Modelview matrix
 var pMatrix = mat4.create(); // Projection matrix
 
 var shaderProgram;
 
-var xRot = 0;
-var xSpeed = 0;
-var yRot = 0;
-var ySpeed = 0;
-var z = -5.0;
-
 var lastTime = 0;
 
-var windowTexture;
-
 var currentlyPressedKeys = {};
+
+var stars = [];
+var starTexture;
+
+var starVertexPositionBuffer;
+var starVertexTextureCoordBuffer;
+
+var zoom = -15;
+var tilt = 90;
+var spin = 0;
+
+var effectiveFPMS = 60/1000;
+
+
+
+function Star(startingDistance, rotationSpeed){
+	this.angle = 0;
+	this.dist = startingDistance;
+	this.rotationSpeed = rotationSpeed;
+
+	// Set the colors to a starting value
+	this.randomizeColors();
+}
+Star.prototype.draw = function(tilt, spin, twinkle){
+	mvPushMatrix();
+	// Move to star's position
+	mat4.rotate(mvMatrix, degToRad(this.angle), [0.0, 1.0, 0.0]);
+	mat4.translate(mvMatrix, [this.dist, 0.0, 0.0]);
+	// Rotate star to face viewer
+	mat4.rotate(mvMatrix, degToRad(-this.angle), [0.0,1.0,0.0]);
+	mat4.rotate(mvMatrix, degToRad(-tilt), [1.0,0.0,0.0]);
+
+	if(twinkle){
+		// draw a non rotating "tinkling colour" star
+		gl.uniform3f(shaderProgram.colorUniform, this.twinkleR, this.twinkleG, this.twinkleB);
+		drawStar();
+	}
+
+	// Spin around the z-axis
+	mat4.rotate(mvMatrix, degToRad(spin), [0.0,0.0,1.0]);
+
+	// Draw the main star colour
+	gl.uniform3f(shaderProgram.colorUniform, this.r, this.g, this.b);
+	drawStar();
+
+	mvPopMatrix();
+};
+Star.prototype.animate = function(elapsedTime){
+	this.angle += this.rotationSpeed * effectiveFPMS * elapsedTime;
+
+	// Move star inward toward center, then reset to outside
+	this.dist -= 0.01 * effectiveFPMS * elapsedTime;
+	if(this.dist < 0.0){
+		this.dist += 5.0;
+		this.randomizeColors();
+	}
+};
+Star.prototype.randomizeColors = function(){
+	this.r = Math.random()*0.8;
+	this.g = Math.random()*0.8;
+	this.b = Math.random()*0.8;
+
+	this.twinkleR = Math.random()*0.8;
+	this.twinkleG = Math.random()*0.8;
+	this.twinkleB = Math.random()*0.8;
+}
+
+function drawStar(){
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, starTexture);
+	gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, starVertexTextureCoordBuffer);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, starVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, starVertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, starVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	setMatrixUniforms();
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, starVertexPositionBuffer.numItems);
+}
+
+
+
+
+function initWorldObjects(){
+	var numStars = 50;
+
+	for(var i=0; i<numStars; i++){
+		stars.push(new Star((i/numStars) * 5.0, i/numStars));
+	}
+}
 
 
 function initGL(canvas){
@@ -56,169 +135,46 @@ function initShaders(){
 	shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
 	gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 
-	shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
-	gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
-
 	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
 	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
 	shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
-	shaderProgram.useLightingUniform = gl.getUniformLocation(shaderProgram, "uUseLighting");
-	shaderProgram.ambientColorUniform = gl.getUniformLocation(shaderProgram, "uAmbientColor");
-	shaderProgram.lightingDirectionUniform = gl.getUniformLocation(shaderProgram, "uLightingDirection");
-	shaderProgram.directionalColorUniform = gl.getUniformLocation(shaderProgram, "uDirectionalColor");
-	shaderProgram.alphaUniform = gl.getUniformLocation(shaderProgram, "uAlpha");
+	shaderProgram.colorUniform = gl.getUniformLocation(shaderProgram, "uColor");
 }
 
 function initBuffers(){
-	// Create the cube vertices and colour buffer
-	cubeVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-	vertices = [
-		// Front face
-		-1.0, -1.0,  1.0,
-		1.0, -1.0,  1.0,
-		1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0, -1.0, -1.0,
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		1.0,  1.0,  1.0,
-		1.0,  1.0, -1.0,
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		1.0, -1.0, -1.0,
-		1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-		// Right face
-		1.0, -1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0,  1.0,  1.0,
-		1.0, -1.0,  1.0,
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0
+	starVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, starVertexPositionBuffer);
+	var vertices = [
+		-1.0, -1.0, 0.0,
+		1.0, -1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0
 	];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	cubeVertexPositionBuffer.itemSize = 3;
-	cubeVertexPositionBuffer.numItems = 24;
+	starVertexPositionBuffer.itemSize = 3;
+	starVertexPositionBuffer.numItems = 4;
 
-	cubeVertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+	starVertexTextureCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, starVertexTextureCoordBuffer);
 	var textureCoords = [
-		// Front face
 		0.0, 0.0,
 		1.0, 0.0,
-		1.0, 1.0,
 		0.0, 1.0,
-
-		// back face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Top face
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-
-		// Bottom face
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-
-		// Right face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Left face
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
+		1.0, 1.0
 	];
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-	cubeVertexTextureCoordBuffer.itemSize = 2;
-	cubeVertexTextureCoordBuffer.numItems = 24;
-
-	cubeVertexNormalBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexNormalBuffer);
-	var vertexNormals = [
-        // Front face
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-
-        // Back face
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-
-        // Top face
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-
-        // Bottom face
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-
-        // Right face
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-
-        // Left face
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
-	cubeVertexNormalBuffer.itemSize = 3;
-	cubeVertexNormalBuffer.numItems = 24;
-
-	cubeVertexIndexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-	var cubeVertexIndices = [
-		0, 1, 2,	0, 2, 3, // Front face
-		4, 5, 6,	4, 6, 7, // Back face
-		8, 9, 10,	8, 10, 11, // Top face
-		12, 13, 14,	12, 14, 15, // Bottom face
-		16, 17, 18, 16, 18, 19, // Right face
-		20, 21, 22, 20, 22, 23 // Left face
-	];
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-	cubeVertexIndexBuffer.itemSize = 1;
-	cubeVertexIndexBuffer.numItems = 36;
+	starVertexTextureCoordBuffer.itemSize = 2;
+	starVertexTextureCoordBuffer.numItems = 4;
 }
 
 function initTexture(){
-	windowTexture = gl.createTexture();
-	windowTexture.image = new Image();
-	windowTexture.image.onload = function(){
-		handleLoadedTexture(windowTexture);
+	starTexture = gl.createTexture();
+	starTexture.image = new Image();
+	starTexture.image.onload = function(){
+		handleLoadedTexture(starTexture);
 	}
 
-	windowTexture.image.src = "stained_glass.jpg";
+	starTexture.image.src = "star.gif";
 }
 
 
@@ -229,8 +185,7 @@ function handleLoadedTexture(texture){
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
 	gl.bindTexture(gl.TEXTURE_2D, null);
 }
@@ -322,26 +277,18 @@ function tick(){
 function handleKeys(){
 	if(currentlyPressedKeys[33]){
 		// Page up
-		z -= 0.05;
+		zoom -= 0.1;
 	}
 	if(currentlyPressedKeys[34]){
 		// Page Down
-		z += 0.05;
-	}
-	if(currentlyPressedKeys[37]){
-		// Left cursor key
-		ySpeed -= 1;
-	}
-	if(currentlyPressedKeys[39]){
-		// Right cursor key
-		ySpeed += 1;
+		zoom += 0.1;
 	}
 	if(currentlyPressedKeys[38]){
 		// Up cursor key
-		xSpeed -= 1;
+		tilt += 2;
 	}
 	if(currentlyPressedKeys[40]){
-		xSpeed += 1;
+		tilt -= 2;
 	}
 }
 
@@ -350,8 +297,9 @@ function animate(){
 	if(lastTime != 0){
 		var elapsed = timeNow - lastTime;
 
-		xRot += (xSpeed * elapsed) / 1000.0;
-		yRot += (ySpeed * elapsed) / 1000.0;
+		for(var i in stars){
+			stars[i].animate(elapsed);
+		}
 	}
 	lastTime = timeNow;
 }
@@ -362,72 +310,18 @@ function drawScene(){
 
 	mat4.perspective(45, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
 
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+	gl.enable(gl.BLEND);
+
 	mat4.identity(mvMatrix);
+	mat4.translate(mvMatrix, [0.0, 0.0, zoom]);
+	mat4.rotate(mvMatrix, degToRad(tilt), [1.0,0.0,0.0]);
 
-	mat4.translate(mvMatrix, [0.0, 0.0, z]);
+	var twinkle = document.getElementById("twinkle").checked;
 
-	mat4.rotate(mvMatrix, degToRad(xRot), [1,0,0]);
-	mat4.rotate(mvMatrix, degToRad(yRot), [0,1,0]);
-
-	// Pass the vertex positions to WebGL
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	// Pass the texture coords to WebGL
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, windowTexture);
-	gl.uniform1i(shaderProgram.samplerUniform, 0);
-
-	// Pass the vertex normals to WebGL
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexNormalBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, cubeVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	var lighting = document.getElementById("lighting").checked;
-	gl.uniform1i(shaderProgram.useLightingUniform, lighting);
-	if(lighting){
-		// Ambient light colour
-		gl.uniform3f(
-			shaderProgram.ambientColorUniform,
-			parseFloat(document.getElementById("ambientR").value),
-			parseFloat(document.getElementById("ambientG").value),
-			parseFloat(document.getElementById("ambientB").value)
-		);
-		// Light direction
-		var lightingDirection = [
-			parseFloat(document.getElementById("lightDirectionX").value),
-			parseFloat(document.getElementById("lightDirectionY").value),
-			parseFloat(document.getElementById("lightDirectionZ").value)
-		];
-		var adjustedLD = vec3.create();
-		vec3.normalize(lightingDirection, adjustedLD);
-		vec3.scale(adjustedLD, -1);
-		gl.uniform3fv(shaderProgram.lightingDirectionUniform, adjustedLD);
-		// Directional light colour
-		gl.uniform3f(
-			shaderProgram.directionalColorUniform,
-			parseFloat(document.getElementById("directionalR").value),
-			parseFloat(document.getElementById("directionalG").value),
-			parseFloat(document.getElementById("directionalB").value)
-		);
-	}
-
-	// Pass the triangle vertex indices (referencing the vertex positions buffer) to WebGL
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-	setMatrixUniforms();
-	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-
-	// Enable blending of transparent materials
-	var blending = document.getElementById("blending").checked;
-	if(blending){
-		gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-		gl.enable(gl.BLEND);
-		gl.disable(gl.DEPTH_TEST);
-		gl.uniform1f(shaderProgram.alphaUniform, parseFloat(document.getElementById("alpha").value));
-	}
-	else{
-		gl.disable(gl.BLEND);
-		gl.enable(gl.DEPTH_TEST);
+	for(var i in stars){
+		stars[i].draw(tilt, spin, twinkle);
+		spin += 0.1;
 	}
 }
 
@@ -449,9 +343,9 @@ function webgl_start() {
 	initShaders();
 	initBuffers();
 	initTexture();
+	initWorldObjects();
 
 	gl.clearColor(0.33, 0.33, 0.33, 1.0); // Approx hex colour of #555555
-	gl.enable(gl.DEPTH_TEST);
 
 	// Paul Irish's gist to polyfill window.requestAnimationFrame
 	(function() {
