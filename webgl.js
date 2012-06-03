f4f1966e031a44045143782d44b67ed130765410
Chris Little <chrislittle1986@gmail.com>
2012-06-02 20:12:08 -0400

    Completed Lesson 14 (Sort of). JSON model loading and specular highlights
    index_base.html is a copy of original lesson code. Code I wrote appears
    to have a sinister issue tranferring from JSON to gl buffer...

--- webgl.js
+++ webgl.js
@@ -4,21 +4,14 @@ var mvMatrixStack = []; // Save and restore previous modelview matrices
 var mvMatrix = mat4.create(); // Modelview matrix
 var pMatrix = mat4.create(); // Projection matrix
 
-var currentProgram;
-var perVertexProgram;
-var perFragmentProgram;
+var shaderProgram;
 
 var moonTexture;
-var moonVertexPositionBuffer;
-var moonVertexTextureCoordBuffer;
-var moonVertexNormalBuffer;
-var moonVertexIndexBuffer;
 
-var boxTexture;
-var boxVertexPositionBuffer;
-var boxVertexTextureCoordBuffer;
-var boxVertexNormalBuffer;
-var boxVertexIndexBuffer;
+var teapotVertexPositionBuffer;
+var teapotVertexNormalBuffer;
+var teapotVertexTextureCoordBuffer;
+var teapotVertexIndexBuffer;
 
 var rotationMatrix = mat4.create();
 mat4.identity(rotationMatrix);
@@ -27,6 +20,46 @@ var mouseDown = false;
 var lastMouseX = null;
 var lastMouseY = null;
 
+
+
+
+function loadTeapot(){
+	var request = new XMLHttpRequest();
+	request.open("GET", "teapot.json");
+	request.onreadystatechange = function(){
+		if(request.readyState == 4){
+			handleLoadedTeapot(JSON.parse(request.responseText));
+		}
+	}
+
+	request.send();
+}
+
+function handleLoadedTeapot(teapotData){
+	teapotVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(teapotData.vertexPositions), gl.STATIC_DRAW);
+	teapotVertexPositionBuffer.itemSize = 3;
+	teapotVertexPositionBuffer.numItems = teapotData.vertexPositions.length / 3;
+
+	teapotVertexNormalBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexNormalBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(teapotData.vertexNormals), gl.STATIC_DRAW);
+	teapotVertexNormalBuffer.itemSize = 3;
+	teapotVertexNormalBuffer.numItems = teapotData.vertexNormals.length / 3;
+
+	teapotVertexTextureCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexTextureCoordBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(teapotData.vertexTextureCoords), gl.STATIC_DRAW);
+	teapotVertexTextureCoordBuffer.itemSize = 2;
+	teapotVertexTextureCoordBuffer.numItems = teapotData.vertexTextureCoords.length / 2;
+
+	teapotVertexIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, teapotVertexIndexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(teapotData.indices), gl.STATIC_DRAW);
+	teapotVertexIndexBuffer.itemSize = 1;
+	teapotVertexIndexBuffer.numItems = teapotData.indices.length;
+}
 
 
 
@@ -42,233 +75,11 @@ function initGL(canvas){
 }
 
 
-function initBuffers(){
-	// Build the sphere
-	var latitudeBands = 30;
-	var longitudeBands = 30;
-	var radius = 1.5;
-
-	var vertexPositionData = [];
-	var normalData = [];
-	var textureCoordData = [];
-	for(var latNumber=0; latNumber <= latitudeBands; latNumber++){
-		var theta = latNumber * Math.PI / latitudeBands;
-		var sinTheta = Math.sin(theta);
-		var cosTheta = Math.cos(theta);
-
-		for(var longNumber=0; longNumber <= longitudeBands; longNumber++){
-			var phi = longNumber * 2 * Math.PI / longitudeBands;
-			var sinPhi = Math.sin(phi);
-			var cosPhi = Math.cos(phi);
-
-			var x = cosPhi * sinTheta;
-			var y = cosTheta;
-			var z = sinPhi * sinTheta;
-			var u = 1 - (longNumber / longitudeBands);
-			var v = 1 - (latNumber / latitudeBands);
-
-			normalData.push(x);
-			normalData.push(y);
-			normalData.push(z);
-			textureCoordData.push(u);
-			textureCoordData.push(v);
-			vertexPositionData.push(radius * x);
-			vertexPositionData.push(radius * y);
-			vertexPositionData.push(radius * z);
-		}
-	}
-
-	var indexData = [];
-	for(var latNumber = 0; latNumber < latitudeBands; latNumber++){
-		for(var longNumber = 0; longNumber < longitudeBands; longNumber++){
-			var first = (latNumber * (longitudeBands + 1)) + longNumber;
-			var second = first + longitudeBands + 1;
-			indexData.push(first);
-			indexData.push(second);
-			indexData.push(first + 1);
-
-			indexData.push(second);
-			indexData.push(second + 1);
-			indexData.push(first + 1);
-		}
-	}
-
-	moonVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexPositionBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositionData), gl.STATIC_DRAW);
-	moonVertexPositionBuffer.itemSize = 3;
-	moonVertexPositionBuffer.numItems = vertexPositionData.length / 3;
-
-	moonVertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexTextureCoordBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
-	moonVertexTextureCoordBuffer.itemSize = 2;
-	moonVertexTextureCoordBuffer.numItems = textureCoordData.length / 2;
-
-	moonVertexNormalBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexNormalBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalData), gl.STATIC_DRAW);
-	moonVertexNormalBuffer.itemSize = 3;
-	moonVertexNormalBuffer.numItems = normalData.length / 3;
-
-	moonVertexIndexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, moonVertexIndexBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
-	moonVertexIndexBuffer.itemSize = 1;
-	moonVertexIndexBuffer.numItems = indexData.length;
-
-
-	// Build the box
-	boxVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexPositionBuffer);
-	var boxVertices = [
-		// Front face
-		-1.0, -1.0,  1.0,
-		1.0, -1.0,  1.0,
-		1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0, -1.0, -1.0,
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		1.0,  1.0,  1.0,
-		1.0,  1.0, -1.0,
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		1.0, -1.0, -1.0,
-		1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-		// Right face
-		1.0, -1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0,  1.0,  1.0,
-		1.0, -1.0,  1.0,
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-	boxVertexPositionBuffer.itemSize = 3;
-	boxVertexPositionBuffer.numItems = 24;
-
-	boxVertexNormalBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexNormalBuffer);
-	var boxNormals = [
-        // Front face
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-         0.0,  0.0,  1.0,
-
-        // Back face
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-         0.0,  0.0, -1.0,
-
-        // Top face
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-         0.0,  1.0,  0.0,
-
-        // Bottom face
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-         0.0, -1.0,  0.0,
-
-        // Right face
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-         1.0,  0.0,  0.0,
-
-        // Left face
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0,
-        -1.0,  0.0,  0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxNormals), gl.STATIC_DRAW);
-	boxVertexNormalBuffer.itemSize = 3;
-	boxVertexNormalBuffer.numItems = 24;
-
-	boxVertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexTextureCoordBuffer);
-	var boxTexCoords = [
-		// Front face
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-
-		// back face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Top face
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-
-		// Bottom face
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-
-		// Right face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Left face
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxTexCoords), gl.STATIC_DRAW);
-	boxVertexTextureCoordBuffer.itemSize = 2;
-	boxVertexTextureCoordBuffer.numItems = 24;
-
-	boxVertexIndexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxVertexIndexBuffer);
-	var boxIndicies = [
-		0, 1, 2,	0, 2, 3, // Front face
-		4, 5, 6,	4, 6, 7, // Back face
-		8, 9, 10,	8, 10, 11, // Top face
-		12, 13, 14,	12, 14, 15, // Bottom face
-		16, 17, 18, 16, 18, 19, // Right face
-		20, 21, 22, 20, 22, 23 // Left face
-	];
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndicies), gl.STATIC_DRAW);
-	boxVertexIndexBuffer.itemSize = 1;
-	boxVertexIndexBuffer.numItems = 36;
-}
-
-
 function initShaders(){
-	perVertexProgram = createProgram("per-vertex-lighting-fs", "per-vertex-lighting-vs");
-	perFragmentProgram = createProgram("per-fragment-lighting-fs", "per-fragment-lighting-vs");
-}
+	var fragmentShader = getShader(gl, "fragment_shader");
+	var vertexShader = getShader(gl, "vertex_shader");
 
-function createProgram(fragmentShaderId, vertexShaderId){
-	var fragmentShader = getShader(gl, fragmentShaderId);
-	var vertexShader = getShader(gl, vertexShaderId);
-
-	var shaderProgram = gl.createProgram();
+	shaderProgram = gl.createProgram();
 	gl.attachShader(shaderProgram, vertexShader);
 	gl.attachShader(shaderProgram, fragmentShader);
 	gl.linkProgram(shaderProgram);
@@ -297,8 +108,9 @@ function createProgram(fragmentShaderId, vertexShaderId){
 	shaderProgram.pointLightingLocationUniform = gl.getUniformLocation(shaderProgram, "uPointLightingLocation");
 	shaderProgram.pointLightingColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingColor");
 	shaderProgram.useTexturesUniform = gl.getUniformLocation(shaderProgram, "uUseTextures");
-
-	return shaderProgram;
+	shaderProgram.useSpecularHighlightingUniform = gl.getUniformLocation(shaderProgram, "uUseSpecularHighlighting");
+	shaderProgram.shininessUniform = gl.getUniformLocation(shaderProgram, "uShininess");
+	shaderProgram.pointLightingSpecularColorUniform = gl.getUniformLocation(shaderProgram, "uPointLightingSpecularColor");
 }
 
 function getShader(gl, id){
@@ -344,14 +156,6 @@ function initTexture(){
 	}
 
 	moonTexture.image.src = "moon.jpg";
-
-	boxTexture = gl.createTexture();
-	boxTexture.image = new Image();
-	boxTexture.image.onload = function(){
-		handleLoadedTexture(boxTexture);
-	}
-
-	boxTexture.image.src = "toon_box.jpg";
 }
 
 function handleLoadedTexture(texture){
@@ -374,14 +178,14 @@ function handleLoadedTexture(texture){
 
 
 function setMatrixUniforms(){
-	gl.uniformMatrix4fv(currentProgram.pMatrixUniform, false, pMatrix);
-	gl.uniformMatrix4fv(currentProgram.mvMatrixUniform, false, mvMatrix);
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
 	// Reverse the effects of the projection and modelview matrix have on the normal vector
 	var normalMatrix = mat3.create();
 	mat4.toInverseMat3(mvMatrix, normalMatrix);
 	mat3.transpose(normalMatrix);
-	gl.uniformMatrix3fv(currentProgram.nMatrixUniform, false, normalMatrix);
+	gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
 }
 
 
@@ -417,105 +221,87 @@ function tick(){
 }
 
 function animate(){
-	mat4.rotate(rotationMatrix, degToRad(0.8), [0,1,0]);
+	mat4.rotate(rotationMatrix, degToRad(0.2), [0,1,0]);
 }
 
 function drawScene(){
 	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	if (teapotVertexPositionBuffer == null || teapotVertexNormalBuffer == null || teapotVertexTextureCoordBuffer == null || teapotVertexIndexBuffer == null) {
+		return;
+	}
+
 	mat4.perspective(45, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
 
 
-	var perFragmentLighting = document.getElementById("per_fragment_lighting").checked;
-	if(perFragmentLighting){
-		currentProgram = perFragmentProgram;
-	}
-	else{
-		currentProgram = perVertexProgram;
-	}
-	gl.useProgram(currentProgram);
+	gl.useProgram(shaderProgram);
 
 	var lighting = document.getElementById("lighting").checked;
-	gl.uniform1i(currentProgram.useLightingUniform, lighting);
+	gl.uniform1i(shaderProgram.useLightingUniform, lighting);
 	if(lighting){
 		// Light position
 		gl.uniform3f(
-			currentProgram.pointLightingLocationUniform,
+			shaderProgram.pointLightingLocationUniform,
 			parseFloat(document.getElementById("lightPositionX").value),
 			parseFloat(document.getElementById("lightPositionY").value),
 			parseFloat(document.getElementById("lightPositionZ").value)
 		);
+		// Point light specular colour
+		gl.uniform3f(
+			shaderProgram.pointLightingSpecularColorUniform,
+			parseFloat(document.getElementById("pointSpecularR").value),
+			parseFloat(document.getElementById("pointSpecularG").value),
+			parseFloat(document.getElementById("pointSpecularB").value)
+		);
 		// Point light colour
 		gl.uniform3f(
-			currentProgram.pointLightingColorUniform,
+			shaderProgram.pointLightingColorUniform,
 			parseFloat(document.getElementById("pointR").value),
 			parseFloat(document.getElementById("pointG").value),
 			parseFloat(document.getElementById("pointB").value)
 		);
 		// Ambient light colour
 		gl.uniform3f(
-			currentProgram.ambientColorUniform,
+			shaderProgram.ambientColorUniform,
 			parseFloat(document.getElementById("ambientR").value),
 			parseFloat(document.getElementById("ambientG").value),
 			parseFloat(document.getElementById("ambientB").value)
 		);
+
+
+		var useSpecularHighlighting = document.getElementById("specular_highlighting").checked;
+		gl.uniform1i(shaderProgram.useSpecularHighlightingUniform, useSpecularHighlighting);
+
+		gl.uniform1f(shaderProgram.shininessUniform, parseFloat(document.getElementById("shininess").value));
 	}
 
 	var useTexturing = document.getElementById("texturing").checked;
-	gl.uniform1i(currentProgram.useTexturesUniform, useTexturing);
+	gl.uniform1i(shaderProgram.useTexturesUniform, useTexturing);
 
 
 	mat4.identity(mvMatrix);
 
-	mat4.translate(mvMatrix, [0,0,-6]);
-	mat4.rotate(mvMatrix, degToRad(45), [1,0,0]);
+	mat4.translate(mvMatrix, [0,0,-40]);
 	mat4.multiply(mvMatrix, rotationMatrix);
 
-	// Draw the moon
-	mvPushMatrix();
-
-	mat4.translate(mvMatrix, [2.5,0,0]);
-
+	// Draw the teapot
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, moonTexture);
-	gl.uniform1i(currentProgram.samplerUniform, 0);
+	gl.uniform1i(shaderProgram.samplerUniform, 0);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexPositionBuffer);
-	gl.vertexAttribPointer(currentProgram.vertexPositionAttribute, moonVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, teapotVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexTextureCoordBuffer);
-	gl.vertexAttribPointer(currentProgram.textureCoordAttribute, moonVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexTextureCoordBuffer);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, teapotVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, moonVertexNormalBuffer);
-	gl.vertexAttribPointer(currentProgram.vertexNormalAttribute, moonVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, teapotVertexNormalBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, teapotVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, moonVertexIndexBuffer);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, teapotVertexIndexBuffer);
 	setMatrixUniforms();
-	gl.drawElements(gl.TRIANGLES, moonVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-
-
-	// Draw the box
-	mvPopMatrix();
-
-	mat4.translate(mvMatrix, [-1.5,0,0]);
-
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
-	gl.uniform1i(currentProgram.samplerUniform, 0);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexPositionBuffer);
-	gl.vertexAttribPointer(currentProgram.vertexPositionAttribute, boxVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexTextureCoordBuffer);
-	gl.vertexAttribPointer(currentProgram.textureCoordAttribute, boxVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexNormalBuffer);
-	gl.vertexAttribPointer(currentProgram.vertexNormalAttribute, boxVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxVertexIndexBuffer);
-	setMatrixUniforms();
-	gl.drawElements(gl.TRIANGLES, boxVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	gl.drawElements(gl.TRIANGLES, teapotVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
 
@@ -525,8 +311,8 @@ function webgl_start() {
 	var canvas = document.getElementById("webgl_canvas");
 	initGL(canvas);
 	initShaders();
-	initBuffers();
 	initTexture();
+	loadTeapot();
 
 	gl.clearColor(0.33, 0.33, 0.33, 1.0); // Approx hex colour of #555555
 	gl.enable(gl.DEPTH_TEST);
